37638c83a06b63e5501ebbecc448ebffb0ed0bcf
iliskhan <iliskhan234@gmail.com>
2021-12-07 12:58:11 +0300

    linked_list(Chores): format with rustfmt

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -9,23 +9,34 @@ struct List<T> {
     tail: Option<Node<T>>,
 }
 
-
 #[cfg(test)]
 mod tests {
     use crate::{List, Node};
 
     #[test]
     fn test_node() {
-        let node = Node { data: "some node data", next: Box::new(None) };
+        let node = Node {
+            data: "some node data",
+            next: Box::new(None),
+        };
         assert_eq!(node.data, "some node data");
         assert!(node.next.is_none());
     }
 
     #[test]
     fn test_list() {
-        let second_node = Node { data: 211, next: Box::new(None) };
-        let first_node = Node { data: 112, next: Box::new(Some(second_node.clone())) };
-        let list = List { head: Some(first_node.clone()), tail: Some(second_node.clone()) };
+        let second_node = Node {
+            data: 211,
+            next: Box::new(None),
+        };
+        let first_node = Node {
+            data: 112,
+            next: Box::new(Some(second_node.clone())),
+        };
+        let list = List {
+            head: Some(first_node.clone()),
+            tail: Some(second_node.clone()),
+        };
         assert!(list.head.is_some());
         if let Some(head) = list.head {
             assert_eq!(head.data, first_node.data);
